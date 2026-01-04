a12172a81b8a2ab7108dd30e45b75c8e25a3a2ac
matteo-cutroni <cutroni.1918336@studenti.uniroma1.it>
2026-01-04 19:56:23 +0100

    google translate

---
+++ js/google_translate.js
@@ -0,0 +1,17 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: 'en', // The language you wrote the site in
+        includedLanguages: 'en,it,es,fr,de', // Languages you want to offer
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+        autoDisplay: false
+    }, 'google_translate_element');
+}
+
+// Auto-load the Google script
+(function() {
+    var googleScript = document.createElement('script');
+    googleScript.type = 'text/javascript';
+    googleScript.async = true;
+    googleScript.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(googleScript);
+})();
